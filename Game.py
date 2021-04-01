9580b93fac0b3f570cf06a432bf001575b1cb2eb
Thornierlemur <70161879+Thornierlemur@users.noreply.github.com>
2021-03-31 23:07:25 -0500

    I added the file Game.py and added some base code to it to try and get pygame to work

--- Game.py
+++ Game.py
@@ -1,1 +1,41 @@
+import pygame 
+import sys
 
+# Setup pygame/window
+mainClock = pygame.time.Clock()
+from pygame.locals import *
+pygame.init()
+pygame.display.set_caption('base case')
+screen = pygame.display.set_mode((500, 500),0,32)
+
+font = pygame.font.SysFont(None, 20)
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+def main_menu():
+    while True:
+
+        screen.fill((0,0,0))
+        draw_text('main menu', font, (255, 255,255), screen, 20, 20)
+
+        for event in pygame.event.get():
+            if event.type == quit:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            
+            pygame.display.update()
+            mainClock.tick(60)
+
+main_menu()
+
+
+##class Game:
+    

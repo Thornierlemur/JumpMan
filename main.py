babb38dd9ac2bcbcfc82671eed6e2925ad2e1321
Thornierlemur <70161879+Thornierlemur@users.noreply.github.com>
2021-04-24 18:45:51 -0500

    Added comments to main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import UserLogin
 #import Game
 
+# call the user login window
 UserLogin.log_in_win()
 
+# Then grab the username if the user logged in
 player = str(UserLogin.get_user_name())
 
-print(player)
-
+# If the player just exited the login window then the "get_user_name()" will return 
+# an empty string indicating that the user did not log in but instead closed the window
 if player == "":
     exit()
 else:
+    # Else the user logged in and we pass their name
     import Game
     Game.main_menu(player)
